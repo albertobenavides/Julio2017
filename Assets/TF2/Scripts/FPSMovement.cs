d652459611dd6b081748215c08ce62a24983d769
Alberto Benavides <bena.87@hotmail.com>
2017-07-06 08:55:20 -0500

    FPS agregado

---
+++ Assets/TF2/Scripts/FPSMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMovement : MonoBehaviour {
+
+    Rigidbody rb;
+    public float force;
+    public float velocity;
+    bool isJumping;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        isJumping = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump") && !isJumping) {
+            rb.AddForce(Vector3.up * force);
+            isJumping = true;
+        }
+
+        
+        rb.velocity = new Vector3(
+            Input.GetAxis("Horizontal") * velocity,
+            rb.velocity.y,
+            Input.GetAxis("Vertical") * velocity
+        );
+
+        transform.Rotate(
+            0,
+            Input.GetAxis("Mouse X"),
+            0
+        );
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag.Equals("Terrain")) {
+            isJumping = false;
+        }
+    }
+}
